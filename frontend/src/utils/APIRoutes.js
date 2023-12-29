6d9bca4b63d5191d568ef06c5f5164c06d1d76b0
VIJAYPRASNTH G <130911218+VIJAYPRASANTHG081104@users.noreply.github.com>
2023-12-29 18:58:24 +0530

    Update APIRoutes.js

--- frontend/src/utils/APIRoutes.js
+++ frontend/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host ="chat-application-api.vercel.app";
+export const host ="https://chat-application-nu-six.vercel.app";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
