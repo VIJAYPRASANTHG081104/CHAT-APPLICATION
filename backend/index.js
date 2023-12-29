f3c3d044658ae8ac16e43b51e39ef4bb84fd6287
VIJAYPRASNTH G <130911218+VIJAYPRASANTHG081104@users.noreply.github.com>
2023-12-29 19:04:34 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,9 +11,12 @@ const socket = require('socket.io');
 
 dotenv.config();
 const app = express();
-app.use(cors({
-    origin:["https://chat-application-client-beta.vercel.app"]
-}));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://chat-application-client-beta.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 app.use(express.json({limit:'10mb'}));
 // app.use(express.urlencoded({limit: '50mb'}));
 PORT = process.env.PORT || 5000
